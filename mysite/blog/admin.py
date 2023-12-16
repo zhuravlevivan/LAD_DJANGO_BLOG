d60a292e16cddac0d8396eab537b9c93ff71d428
legion <855434@gmail.com>
2023-12-16 20:25:54 +0300

    16.12.2023

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,4 +7,6 @@ class PostAdmin(admin.ModelAdmin):
 	list_display = ['title', 'slug', 'author', 'publish', 'status']
 	list_filter = ['status', 'created', 'publish', 'author']
 	search_fields = ['title', 'body']
-
+	prepopulated_fields = {'slug': ('title',)}
+	ordering = ['-status', 'publish']
+	raw_id_fields = ['author']
